9cc1d45251d53c378c2a2cb05905bcbfd7d8e3d5
Simon  Date <simondate@yahoo.co.uk>
2017-06-12 14:55:23 +0100

    Created schema

--- js/adapt-component-icon.js
+++ js/adapt-component-icon.js
@@ -30,15 +30,12 @@ define([
     },
 
     applyImage: function($el, src) {
-      console.log($($el).find('.component-header'));
       $($el).find('.component-header').prepend("<div class='component-icon-holder'><img class='component-icon' src='"+ src + "'/></div>");
     }
   });
 
   Adapt.on("pageView:postRender", function(view) {
     var model = view.model;
-    console.log(model.findDescendants('components'));
-    console.log(model);
       new ComponentIconView({
         model: model,
         el: view.el
