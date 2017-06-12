df9f8c5946fb537659957f7f026419291e98ad5d
Simon  Date <simondate@yahoo.co.uk>
2017-06-12 14:49:59 +0100

    Deleted code from other extension.

--- js/adapt-component-icon.js
+++ js/adapt-component-icon.js
@@ -8,35 +8,22 @@ define([
 
     initialize: function() {
       var model = this.model;
-      console.log(this.model.findDescendants('components'));
       this._componentIcons = this.model.findDescendants('components').filter(function(models) {
         return models.get("_componentIcon");
       });
       if (this._componentIcons.length == 0) {
         return;
       }
-      this._articleModelsIndexed = _.indexBy(this._articleModels, "_id");
 
       this.listenTo(Adapt, "pageView:ready", this.onPageReady);
-
-      //Is this the best way to swap out graphics - maybe best to add both graphics on load and then toggle classes on device:changed, device:resize?
-      this.listenTo(Adapt, 'device:changed', this.onPageReady);
-      this.listenTo(Adapt, 'device:resize', this.onPageReady);
     },
 
     onPageReady: function() {
-      this.$articleElements = {};
-      this.callbacks = {};
-      console.log(this._componentIcons);
       for (var i = 0, l = this._componentIcons.length; i < l; i++) {
         var componentModel = this._componentIcons[i];
-        if (!componentModel.get('_componentIcon')) continue;
         if (componentModel.get('_componentIcon').src !== "") {
           var id = componentModel.get("_id");
-          console.log(id);
-
           var $componentElement = this.$el.find("." + id);
-
           this.applyImage($componentElement, componentModel.get('_componentIcon').src);
         }
       }
