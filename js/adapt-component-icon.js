aa0c4ab45cfd00981723e327cdc0fd72c521331a
Simon  Date <simondate@yahoo.co.uk>
2017-06-29 15:37:53 +0100

    Uses underscore instead of for loop

--- js/adapt-component-icon.js
+++ js/adapt-component-icon.js
@@ -11,22 +11,18 @@ define([
       this._componentIcons = this.model.findDescendants('components').filter(function(models) {
         return models.get("_componentIcon");
       });
-      if (this._componentIcons.length == 0) {
-        return;
-      }
 
       this.listenTo(Adapt, "pageView:ready", this.onPageReady);
     },
 
     onPageReady: function() {
-      for (var i = 0, l = this._componentIcons.length; i < l; i++) {
-        var componentModel = this._componentIcons[i];
+      _.each(this._componentIcons, function(componentModel) {
         if (componentModel.get('_componentIcon').src !== "") {
           var id = componentModel.get("_id");
           var $el = this.$el.find("." + id);
           this.applyImage($el, componentModel.get('_componentIcon'));
         }
-      }
+      }, this);
     },
 
     applyImage: function($el, componentIcon) {
@@ -37,10 +33,10 @@ define([
 
   Adapt.on("pageView:postRender", function(view) {
     var model = view.model;
-      new ComponentIconView({
-        model: model,
-        el: view.el
-      });
+    new ComponentIconView({
+      model: model,
+      el: view.el
+    });
   });
 
 });
